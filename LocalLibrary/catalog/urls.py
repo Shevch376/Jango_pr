408ee8b5a4b7535124c876f0756b03d475ed3aee
Shevch376 <symaexplay5430@gmail.com>
2024-11-21 02:38:01 +0700

    commit3

--- LocalLibrary/catalog/urls.py
+++ LocalLibrary/catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # Путь для главной страницы
+    path('books/', views.BookListView.as_view(), name='books'),  # Путь для списка книг
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Путь для деталей книги
 ]
+
+
